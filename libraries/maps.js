39eacaab0248ae405d05ff5ea3ca2a53faca5f14
96rus <kruchininruslan@mail.ru>
2022-08-02 20:06:12 +0500

    Удаление ненужных для обучения файлов

--- libraries/maps.js
+++
@@ -1,9 +0,0 @@
-import openGoogle from '../google.js';
-import openAllMaps from '../src/main.js';
-import openYandex from './yandex.js';
-
-let openAllMaps = () => {
-  openGoogle();
-  openYandex();
-  openAllMaps();
-}
--- libraries/yandex.js
+++
@@ -1,6 +0,0 @@
-let openYandex = () => {
-  //connect to Yandex
-  // show map // modules AMD require.js
-}
-
-export default openYandex;
